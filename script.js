44cd197fb69e70e80d8400ba2b5a8dd685dc72ee
TradAlgoio13 <mrleo3356@gmail.com>
2026-08-01 08:15:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+alert("Script Loaded");
+function copyWallet() {
+    let wallet = document.getElementById("wallet");
+    wallet.select();
+    document.execCommand("copy");
+    alert("Wallet Address Copied!");
+}
+
+const popup = document.getElementById("depositPopup");
+
+// Open Popup
+function openPopup() {
+    popup.style.display = "flex";
+}
+
+// Close Popup
+function closePopup() {
+    popup.style.display = "none";
+
+    document.getElementById("depositForm").style.display = "block";
+    document.getElementById("loadingMessage").style.display = "none";
+    document.getElementById("successMessage").style.display = "none";
+
+    document.getElementById("depositForm").reset();
+}
+
+// Close popup when clicking outside
+window.onclick = function(event) {
+    if (event.target === popup) {
+        closePopup();
+    }
+};
+
+// Handle form submission
+document.getElementById("depositForm").addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    document.getElementById("depositForm").style.display = "none";
+    document.getElementById("loadingMessage").style.display = "block";
+
+    setTimeout(function(){
+
+        document.getElementById("loadingMessage").style.display = "none";
+        document.getElementById("successMessage").style.display = "block";
+
+    },2000);
+
+});
